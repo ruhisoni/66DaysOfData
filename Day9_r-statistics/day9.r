e32531fac420606c081ba6239645d5c2e28c1b1f
ruhisoni <ruhisoni15@gmail.com>
2023-05-08 22:43:47 +0530

    Updated Day9 R code

--- Day9_r-statistics/day9.r
+++ Day9_r-statistics/day9.r
@@ -25,3 +25,36 @@ rdu_flights %>%
 #Filtering for flights headed to San Francisco (SFO) in February.
 sfo_feb_flights <- nycflights %>%
     filter(dest == "SFO", month == 2)
+
+#Obtaining basic statistical measures of delay times of flights headed to RDU, grouped by airport of origin in NYC.
+rdu_flights %>%
+    group_by(origin)  %>% 
+    summarise(mean_dd = mean(dep_delay), sd_dd = sd(dep_delay))
+
+#Finding out which month had the highest average flight departure delay time.
+nycflights  %>% 
+    group_by(month)  %>% 
+    summarise(mean_dd = mean(dep_delay)) %>% 
+    arrange(desc(mean_dd))
+
+ggplot(nycflights, aes(x = factor(month), y = dep_delay)) +
+    geom_boxplot()
+
+#Using the ifelse function to classify flight departures as "on time" or "delayed".
+nycflights <- nycflights  %>% 
+    mutate(dep_type = ifelse(dep_delay < 5, "on time", "delayed"))
+
+#Finding the proportion of on-time departures at each NYC airport.
+nycflights  %>% 
+    group_by(origin)  %>% 
+    summarise(ot_dep_rate = sum(dep_type == "on time") / n())  %>% 
+    arrange(desc(ot_dep_rate))
+
+ggplot(data = nycflights, aes(x = origin, fill = dep_type)) +
+    geom_bar()
+
+#Adding a variable of average flight speed to the dataframe.
+nycflights <- nycflights  %>% 
+    mutate(avg_speed = (distance/air_time)/60)
+
+#Note: In accordance with Coursera's Honor Code, some lines of code -- namely, quiz answers -- have been removed.
